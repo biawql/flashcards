7a43ca2020eee39e1920cf072e4289d78c7a7c76
Bia 💭 <biancabaglie18@gmail.com>
2024-11-24 20:45:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Disney',
+    'Qual o lema do Rei Leão, que significa "sem preocupações"?',
+    'Hakuna Matata'
+)
+
+criaCartao(
+    'Disney',
+    'Qual objeto mágico ajuda Cinderela a ir ao baile?',
+    'A carruagem feita a partir de uma abóbora'
+)
+
+criaCartao(
+    'Pixar',
+    'Em Procurando Nemo, qual é o nome do peixe que ajuda Marlin a encontrar seu filho?',
+    'Dory'
+)
+
+criaCartao(
+    'Pixar',
+    'Qual sentimento que assume o controle em Divertida Mente quando Riley enfrenta mudanças em sua vida?',
+    'Tristeza'
+)
+
+criaCartao(
+    'DreamWorks',
+    'Em Shrek, o que Shrek ganha depois de salvar a princesa?',
+    'O título de herói e seu pântano de volta'
+)
+
+criaCartao(
+    'DreamWorks',
+    'Qual música é famosa na cena final de Madagascar, onde os animais dançam?',
+    '"I Like to Move It"'
+)
